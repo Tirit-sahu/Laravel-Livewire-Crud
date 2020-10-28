e8aa0193bdd6cea02b420094b6667f4a54941501
Tirit Sahu <tirithesahu@gmail.com>
2020-10-29 01:16:59 +0530

    home page bug solve

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    @livewireStyles
 </head>
 <body>
     <div class="container">
@@ -13,7 +14,7 @@
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h2>Laravel Livewire Crud - Tirit Sahu</h2>
+                        <h2>Laravel Livewire Crud - Tirit sahu</h2>
                     </div>
                     <div class="card-body">
                         @if (session()->has('message'))
@@ -27,5 +28,6 @@
             </div>
         </div>
     </div>
+    @livewireScripts
 </body>
 </html>
